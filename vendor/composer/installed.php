17f66653e7ccfced09846b8817091289377a2e6b
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-25 13:23:15 -0300

    adiciona a pasta vendor no repositorio

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bb92fa30b8de00fa7b609c1592650356978957ed',
+        'name' => 'matheusolivr/tabela-controle-funcionario',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'matheusolivr/tabela-controle-funcionario' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bb92fa30b8de00fa7b609c1592650356978957ed',
+            'dev_requirement' => false,
+        ),
+    ),
+);

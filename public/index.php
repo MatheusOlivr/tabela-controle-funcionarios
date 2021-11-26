e5471383a797dea29eb4e77d1169cf37947c2b50
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-26 09:11:56 -0300

    Corrige o código do slim no arquivo index

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php 
+    use \Slim\Slim;
+    require "../vendor/autoload.php";
     $app = new \Slim\Slim();
-    $app->get('/hello/:name', function ($name) {
-        echo "Hello, " . $name;
+    $app->get('/', function () {
+        echo "HOME";
     });
     $app->run();
 ?>
